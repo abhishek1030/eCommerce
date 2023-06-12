ed3a12fd2e8b3a737dbf0dce564eb0ccf2805f2a
Abhishek Anand <anand007abhishek@gmail.com>
2023-06-12 17:44:23 +0530

    Added discovery server and modified product,order and inventory to client

--- product-service/src/main/java/com/abhishekanand/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/abhishekanand/productservice/ProductServiceApplication.java
@@ -5,9 +5,11 @@ import com.abhishekanand.productservice.model.Product;
 import com.abhishekanand.productservice.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args)
